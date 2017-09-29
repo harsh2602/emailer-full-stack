35f69f0ca0c562766dc5ac70dbf9edc2b5f6aab7
Harsh Khandelwal <harshkhandelwal@Harshs-MacBook-Pro.local>
2017-09-29 02:59:37 -0500

    refactored after adding new services and routes folders

--- server/index.js
+++ server/index.js
@@ -1,48 +1,10 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy; // Passport Docs: OAuthStrategy
-const keys = require('../config/keys');
+require('./services/passport');
 
 // Create an instance of the application
 const app = express();
 
-// Use the GoogleStrategy within Passport.
-//  Strategies in passport require a `verify` function, which accept
-//  credentials (in this case, a token, tokenSecret, and Google profile), and
-//  invoke a callback with a user object.
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log('access token', accessToken);
-      console.log('refresh token', refreshToken);
-      console.log('profile', profile);
-    }
-  )
-);
-
-// GET /auth/google
-//   Use passport.authenticate() as route middleware to authenticate the
-//   request.  The first step in Google authentication will involve redirecting
-//   the user to google.com.  After authorization, Google will redirect the user
-//   back to this application at /auth/google/callback
-app.get(
-  '/auth/google',
-  passport.authenticate('google', {
-    scope: ['profile', 'email']
-  })
-);
-
-// GET /auth/google/callback
-//   Use passport.authenticate() as route middleware to authenticate the
-//   request.  If authentication fails, the user will be redirected back to the
-//   login page.  Otherwise, the primary route function function will be called,
-//   which, in this example, will redirect the user to the home page.
-app.get('/auth/google/callback', passport.authenticate('google'));
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
