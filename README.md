45fd2df72e291bebe65a293043f95a1f9a859f3a
Harsh Khandelwal <harshkhandelwal@Harshs-MacBook-Pro.local>
2017-09-29 01:22:19 -0500

    Added Passport and Refactored Project

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # emailer-full-stack
-Full stack project involving usage of Node, MongoDB, Express, React and Redux
+Full stack project involving usage of Node, MongoDB, Express,Passport, React and Redux
+
+# Passport
+General helpers for handling auth in Express apps
+
+# Passport Strategy
+Helpers for authenticating with one very specific method (email/password, Google, Facebook, etc)
+
+NOTE: Only using version 2.0 for google oauth(passport-google-oauth20).
--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+// Create an instance of the application
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({
-    hi: 'Harsh'
-  });
-});
+// Use the GoogleStrategy within Passport.
+// Strategies in passport require a `verify` function, which accept
+// credentials (in this case, a token, tokenSecret, and Google profile), and
+// invoke a callback with a user object.
+passport.use(new GoogleStrategy());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
