2cdb5bc70aef09e6219fe66e832d19c11d96d4e7
Harsh Khandelwal <harshkhandelwal@Harshs-MacBook-Pro.local>
2017-09-29 02:15:11 -0500

    added google oauth

--- server/index.js
+++ server/index.js
@@ -1,15 +1,39 @@
 const express = require('express');
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const GoogleStrategy = require('passport-google-oauth20').Strategy; // Passport Docs: OAuthStrategy
+const keys = require('../config/keys');
 
 // Create an instance of the application
 const app = express();
 
 // Use the GoogleStrategy within Passport.
-// Strategies in passport require a `verify` function, which accept
-// credentials (in this case, a token, tokenSecret, and Google profile), and
-// invoke a callback with a user object.
-passport.use(new GoogleStrategy());
+//  Strategies in passport require a `verify` function, which accept
+//  credentials (in this case, a token, tokenSecret, and Google profile), and
+//  invoke a callback with a user object.
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
+
+// GET /auth/google
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  The first step in Google authentication will involve redirecting
+//   the user to google.com.  After authorization, Google will redirect the user
+//   back to this application at /auth/google/callback
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
