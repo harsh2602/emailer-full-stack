8658d54662286e444490b5575ed942f45b4b7bbf
Harsh Khandelwal <harshkhandelwal@Harshs-MacBook-Pro.local>
2017-09-30 14:48:50 -0500

    added current user and logout routes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -13,10 +13,20 @@ module.exports = app => {
     })
   );
 
+  app.get('/api/logout', (req, res) => {
+    req.logout(); // Attached to the request object by passport
+    res.send(req.user);
+  });
+
   // GET /auth/google/callback
   //   Use passport.authenticate() as route middleware to authenticate the
   //   request.  If authentication fails, the user will be redirected back to the
   //   login page.  Otherwise, the primary route function function will be called,
   //   which, in this example, will redirect the user to the home page.
   app.get('/auth/google/callback', passport.authenticate('google'));
+
+  // GET
+  app.get('/api/current_user', (req, res) => {
+    res.send(req.user); // Passport attached the user property to the request object
+  });
 };
