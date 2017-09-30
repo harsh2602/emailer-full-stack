633b02ac0c68fa909a61357c330133c70eaa9e48
Harsh Khandelwal <harshkhandelwal@Harshs-MacBook-Pro.local>
2017-09-30 14:47:22 -0500

    added cookie-session and passport middleware

--- server/index.js
+++ server/index.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session'); // Manages cookies for the session
+const passport = require('passport'); // Passport here needs to keep track of user authentication state by using cookies
 const keys = require('../config/keys');
+
+/**
+* Order important for the two require statement to avoid:
+* MissingSchemaError: Schema hasn't been registered for model
+*/
+require('./models/User');
 require('./services/passport');
 
 // Connect to MongoDB
@@ -8,6 +16,19 @@ mongoose.connect(keys.mongoURI);
 
 // Create an instance of the application
 const app = express();
+
+// Express needs to make use of cookies for the application
+app.use(
+  // Cookie Session will take the passed object and assign it to req.session property
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey] //key is used to encrypt a cookie. Multiple allowed and one will be picked randomly.
+  })
+);
+
+// Passport makes use of cookies to handle authentication
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
