273c42dc02b40607c3368c8667d8cff691cd04e6
Harsh Khandelwal <harshkhandelwal@Harshs-MacBook-Pro.local>
2017-09-29 22:28:58 -0500

    added mongoose connection

--- server/index.js
+++ server/index.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('../config/keys');
 require('./services/passport');
+
+// Connect to MongoDB
+mongoose.connect(keys.mongoURI);
 
 // Create an instance of the application
 const app = express();
