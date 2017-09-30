bd0b064acf87b3be899e7780fe3672b4a76b15a4
Harsh Khandelwal <harshkhandelwal@Harshs-MacBook-Pro.local>
2017-09-30 14:48:16 -0500

    added serialized and deserialized function

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,6 +1,31 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy; // Passport Docs: OAuthStrategy
+const mongoose = require('mongoose');
 const keys = require('../../config/keys');
+// Fetch the users model
+const User = mongoose.model('users');
+
+/**
+* @function serializeUser generate an identifying piece of information. That information will be stuffed as a cookie to the header.
+* @param user the user object is a single instance from the users collection
+* @param done signifies to passport middleware that process is done with id of user returned
+*/
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+/**
+* @function deSerializeUser take the id from cookie and turn it back into actual user model
+* @param id is the user id that was generated when user model was serialized
+*/
+passport.deserializeUser((id, done) => {
+  /**
+  * @function findById find a record using the id(similar to primary key)
+  */
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
 
 // Use the GoogleStrategy within Passport.
 //  Strategies in passport require a `verify` function, which accept
@@ -13,10 +38,24 @@ passport.use(
       clientSecret: keys.googleClientSecret,
       callbackURL: '/auth/google/callback'
     },
+    // Create a new user in MongoDB
     (accessToken, refreshToken, profile, done) => {
-      console.log('access token', accessToken);
-      console.log('refresh token', refreshToken);
-      console.log('profile', profile);
+      /**
+      * @function findOne: Find one record based on a condition
+      * @return Promise
+      * @param existingUser: is an object i.e. either a MongooseModel Instance or a null object
+      */
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          // User already exists with the ID
+          done(null, existingUser); // Communicate to passport that process it finished
+        } else {
+          // Create new user with new ID
+          new User({ googleId: profile.id }) // Represents a model instance
+            .save()
+            .then(user => done(null, user)); // Represents another model instance although a similar one to the previous
+        }
+      });
     }
   )
 );
