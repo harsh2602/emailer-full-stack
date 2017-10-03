63599f3a1bf74fa633f0ebfd0f3648c307af85b8
Harsh Khandelwal <harshkhandelwal@Harshs-MacBook-Pro.local>
2017-10-03 00:53:35 -0500

    updated fetchUser

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,9 +1,5 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 
-export const fetchUser = () => async dispatch => {
-  // dispatch is a function
-  const res = await axios.get('/api/current_user');
-
-  dispatch({ type: FETCH_USER, payload: res });
-};
+export const fetchUser = () => async dispatch =>
+  dispatch({ type: FETCH_USER, payload: await axios.get('/api/current_user') });
