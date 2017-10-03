f192f985e66d3429e1ba7ce6b1315eee00911980
Harsh Khandelwal <harshkhandelwal@Harshs-MacBook-Pro.local>
2017-10-03 00:51:50 -0500

    added fetchUser action

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 import Header from './Header';
 
-const App = () => {
-  return (
-    <div>
-      <BrowserRouter>
-        <div className="container">
-          <Header />
-          {/* <Route exact path="/" component={Landing} />
-          <Route exact path="/surveys" component={Dashboard} />
-          <Route path="/surveys/new" component={SurveyNew} /> */}
-        </div>
-      </BrowserRouter>
-    </div>
-  );
-};
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
 
-export default App;
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <div className="container">
+            <Header />
+            {/* <Route exact path="/" component={Landing} />
+            <Route exact path="/surveys" component={Dashboard} />
+            <Route path="/surveys/new" component={SurveyNew} /> */}
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+export default connect(null, actions)(App);
--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
 import App from './components/App';
 import reducers from './reducers';
 
@@ -12,7 +13,7 @@ import reducers from './reducers';
 * @param initialState
 * @param applyMiddleware
 */
-const store = createStore(reducers, {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
   <Provider store={store}>
