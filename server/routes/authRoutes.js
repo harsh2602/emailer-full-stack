f67c625d7c8307b6b6185114aab27c392620dfe6
Harsh Khandelwal <harshkhandelwal@Harshs-MacBook-Pro.local>
2017-10-03 22:17:59 -0500

    updated comments

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -20,9 +20,7 @@ module.exports = app => {
 
   // GET /auth/google/callback
   //   Use passport.authenticate() as route middleware to authenticate the
-  //   request.  If authentication fails, the user will be redirected back to the
-  //   login page.  Otherwise, the primary route function function will be called,
-  //   which, in this example, will redirect the user to the home page.
+  //   request.
   app.get('/auth/google/callback', passport.authenticate('google'));
 
   // GET
