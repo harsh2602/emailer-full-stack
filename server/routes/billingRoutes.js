40faa65605ad64c3a3be170ab7ef45f6f7da39c7
Harsh Khandelwal <harshkhandelwal@Harshs-MacBook-Pro.local>
2017-10-09 14:48:13 -0500

    updated billing routes with callback

--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -1,16 +1,37 @@
 const keys = require('../../config/keys');
+console.log(keys.stripeSecretkey);
 const stripe = require('stripe')(keys.stripeSecretkey);
 
-module.exports = app => {
-  app.post('/api/stripe', async (req, res) => {
-    // Create an actual charge from the token in the req.body
-    const charge = await stripe.charges.create({
-      amount: 500,
-      currency: 'usd',
-      description: '$5 dollars for 5 credits',
-      source: req.body.id
-    });
+// module.exports = app => {
+//   app.post('/api/stripe', async (req, res) => {
+//     // Create an actual charge from the token in the req.body
+//     const charge = await stripe.charges.create({
+//       amount: 500,
+//       currency: 'usd',
+//       source: req.body.id, // obtained with Stripe.js
+//       description: '$5 for 5 email credits'
+//     });
+//
+//     access current user model available on request object from passport
+//     req.user.credits += 5;
+//     const user = await req.user.save(); // When saving call the user model again(the updated one)
+//     res.send(user);
+//   });
+// };
 
-    console.log(charge);
+module.exports = app => {
+  app.post('/api/stripe', (req, res) => {
+    stripe.charges.create(
+      {
+        amount: 500,
+        currency: 'usd',
+        source: req.body.id, // obtained with Stripe.js
+        description: '$5 for 5 email credits'
+      },
+      (err, charge) => {
+        console.log('Error:', err);
+        console.log('Charge:', charge);
+      }
+    );
   });
 };
